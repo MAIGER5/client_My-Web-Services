63ca02dcae9c598a82261b76cf9e61b5e1cfdb15
maiger5 <maiger5learning@gmail.com>
2025-01-06 23:49:05 -0500

    empezando el primer componente de home

--- client_My-Web-Services/src/views/home/components/screenFirst/screenFirts.jsx
+++ client_My-Web-Services/src/views/home/components/screenFirst/screenFirts.jsx
@@ -4,7 +4,13 @@ import styles from './screenFirts.module.css'
 export const ScreenFirts = () => {
   return (
     <div className={styles.container}>
-      hola mundo
+      <div>
+        <div className={styles.logoInterno}></div>
+        <span>K&WEB</span>
+      </div>
+      <div>
+        <img src="https://sis-it.com/_ipx/w_2112&q_100/https://imgsitesis.blob.core.windows.net/imgsitesis/fc172678-fe9a-4bcf-a249-b5a5edc9cb56.webp" alt="" />
+      </div>
     </div>
   )
 }
